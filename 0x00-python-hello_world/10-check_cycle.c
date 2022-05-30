1f659cf1145efd3ed4091c141c3d0f9732ae27c7
bete7512 <betekbebe@gmail.com>
2022-05-30 12:23:21 -0700

    hello

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,24 +1,28 @@
 #include "lists.h"
 /**
- *
- *
- *
+ *check_cycle - function for checking cycles in singly linked list
+ *@list:lists as parameter to be checked
+ *Return:integer
  *
  */
 
 int check_cycle(listint_t *list)
 {
 	listint_t *head;
-	if(list == NULL)
+	listint_t *fast;
+
+	if (list == NULL)
 		return (0);
 	head = list;
-	while(list != NULL)
+	fast = list;
+	while (head && fast && fast->next)
 	{
-		if(list->next == head)
+		head = head->next;
+		fast = fast->next->next;
+		if (head == fast)
 		{
 			return (1);
 		}
-		list = list->next;
 	}
 	return (0);
 
