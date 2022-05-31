17a2ea7a4fbfefb23a9ab7d5d9dbbb3f51bb9f01
bete7512 <betekbebe@gmail.com>
2022-05-31 10:46:38 -0700

    the same forever

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for c in range(ord('a'), ord('z')+1):
-    if c == 'q' or c == 'e':
+    if c == ord('q') or c == ord('e'):
         continue
     print("{:c}".format(c), end="")
