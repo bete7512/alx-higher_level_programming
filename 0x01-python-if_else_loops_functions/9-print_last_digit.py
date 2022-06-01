1783507ef8443517eed5cddd3750381d66c03177
bete7512 <betekbebe@gmail.com>
2022-06-01 09:35:16 -0700

    no more

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number >= 0:
+        num =  number % 10
+    else:
+        number = number * -1
+        num = number % 10
+    print(num, end='')
+    return num
