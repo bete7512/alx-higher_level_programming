b95422511de3176f61616c3c07f21ff7df17e976
bete7512 <betekbebe@gmail.com>
2022-06-01 09:47:26 -0700

    hero

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number >= 0:
-        num =  number % 10
+        num = number % 10
     else:
         number = -number
         num = number % 10
