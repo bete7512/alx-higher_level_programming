83230a763484b6bc180e6d401af28d52defa845b
bete7512 <betekbebe@gmail.com>
2022-06-02 12:50:33 -0700

    measure

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-num = len(sys.argv)
-sum = 0
-if num - 1 == 0:
-    print("0")
-else:
-    for i in range(1, num):
-        sum += int(sys.argv[i])
-    print(sum)
+if __name__ == "__main__":
+    import sys
+    num = len(sys.argv)
+    sum = 0
+    if num - 1 == 0:
+        print("0")
+    else:
+        for i in range(1, num):
+            sum += int(sys.argv[i])
+        print(sum)
