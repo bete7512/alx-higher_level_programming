c1e100b942e5bfbb97bf0cd511700e562286df2d
bete7512 <betekbebe@gmail.com>
2022-06-02 14:25:04 -0700

    special error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    from calculator_1 import add, sub, div, mul
     num = len(sys.argv)
     if num != 4:
         exit(1)
+    op = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if op == '+':
+        print("{} {} {} = {}".format(a, op, b, add(a, b)))
+    elif op == '/':
+        print("{} {} {} = {}".format(a, op, b, div(a, b)))
+    elif op == '*':
+        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
+    elif op == '-':
+        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
     else:
-        ch = sys.argv[2]
-        if ch == '+':
-            sum = int(sys.argv[1]) + int(sys.argv[3])
-            print("{} {} {} = {}".format(int(sys.argv[1]), ch, int(sys.argv[3]), sum))
-        elif ch == '*':
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-            mul = a * b
-            print("{} {} {} = {}".format(a, ch, b, mul))
-        elif ch == '/':
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-            div = a / b
-            print("{} {} {} = {}".format(a, ch, b, div))
-        elif ch == '-':
-            sub = int(sys.argv[1]) - int(sys.argv[3])
-            print("{} {} {} = {}".format(int(sys.argv[1]), ch, int(sys.argv[3]), sub))
-        else:
-            print("enter valid operator")
-            exit(1)
+        print("enter valid operator")
+        exit(1)
