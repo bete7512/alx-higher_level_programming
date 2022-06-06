bfafdab911e80236d9f5b6e23d66c580d8cfe381
bete7512 <betekbebe@gmail.com>
2022-06-06 14:08:04 -0700

    no one is here

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-        for row in matrix:
-                for col in row:
-                        print("{:d}".format(col), end=" ")
-                print()
+    """Print a matrix of integers."""
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end="")
+                if j != (len(matrix[i]) - 1):
+                    print(" ", end="")
+
+        print("")
