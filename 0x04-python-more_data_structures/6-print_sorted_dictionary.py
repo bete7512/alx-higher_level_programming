1a14b1512df6044016f839c18a12e316073f1779
bete7512 <betekbebe@gmail.com>
2022-06-08 10:43:07 -0700

    hello this is oxo4

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    x = sorted(a_dictionary.items())
+    x = sorted(a_dictionary)
     for key, value in x:
         print(key, ":", value)
