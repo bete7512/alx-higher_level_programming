7d25ede9d6e7ed280841ce1de7af8d9a051b865b
bete7512 <betekbebe@gmail.com>
2022-09-05 13:31:46 +0300

    again

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
 'use strict';
-let words = 'C is fun';
-console.log(words);
-words = 'Python is cool';
-console.log(words);
-words = 'Javascript is amazing';
-console.log(words);
-// #!/usr/bin/node
-// 'use strict';
-// console.log("C is fun \nPython is cool \nJavaScript is amazing");
+console.log("C is fun \nPython is cool \nJavaScript is amazing");
