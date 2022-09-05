96e736558de2f2f89b14d071b8418a93f7352ec5
bete7512 <betekbebe@gmail.com>
2022-09-05 13:29:28 +0300

    javascript warm up

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 'use strict';
-const myVar = 'Javascript is amazing';
+const myVar = "JavaScript is amazing";
 console.log(myVar);
-// #!/usr/bin/node
-// 'use strict';
-// const myVar = "JavaScript is amazing";
-// console.log(myVar);
