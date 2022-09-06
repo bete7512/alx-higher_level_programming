3cba213e1e0d026a871ee009c2018e95314a841d
bete7512 <betekbebe@gmail.com>
2022-09-06 21:36:26 +0300

    class and object in javascript

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+module.exports = class Rectangle{
+    constructor(w,h){
+        if(w > 0 && h > 0){
+            this.width = w;
+            this.height = h;
+        }
+    };
+
+    print (){
+        let line = '';
+        for (let i = 0;i<this.width;i++){
+        line += 'X';
+        }
+        for (let i = 0;i<this.height;i++){
+            console.log(line);
+        }
+    }
+}
