1af98b7e9145039760082ad9e067450143a507f3
bete7512 <betekbebe@gmail.com>
2022-09-07 20:30:44 +0300

    coder

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-'use strict';
 exports.esrever = function (list) {
-  const hold = [];
-  list.forEach((element) => {
-    hold.push(element);
-  });
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
