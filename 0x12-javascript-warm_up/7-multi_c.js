2901b4f515f31d8fb55e80c7085443976c4c89bc
bete7512 <betekbebe@gmail.com>
2022-09-07 17:50:53 +0300

    go there now

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 'use strict';
 const words = 'C is fun';
-if (!process.argv[2]) {
-  console.log('Missing number of occurrences');
-}
 const j = process.argv[2];
 let i = 0;
 while (i < j) {
