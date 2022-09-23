b313c3dc34596b3972dc71d61b1ecc72910fcec0
bete7512 <betekbebe@gmail.com>
2022-09-23 22:38:50 +0300

    commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status."""
 import urllib.request
-
-
 if __name__ == "__main__":
     request = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
