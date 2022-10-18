1e721d150c4b5beeb516eec38d9933ae5d75db78
bete7512 <betekbebe@gmail.com>
2022-10-18 22:51:20 +0300

    commited

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+'use strict';
+const request = require('request');
+
+request(process.argv[2], { json: true }, (error, response, data) => {
+  if (error) console.log(error); else {
+    if (response.statusCode === 200) {
+      const todos = {};
+
+      data.forEach(todo => {
+        if (todos[todo.userId] === undefined) todos[todo.userId] = 0;
+        if (todo.completed === true) todos[todo.userId]++;
+      });
+      console.log(todos);
+    }
+  }
+});
