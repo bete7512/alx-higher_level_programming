c6ab21a0a440bc921a8d8cd863c5f2293525c063
bete7512 <betekbebe@gmail.com>
2022-10-18 20:33:30 +0300

    javascript file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,10 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2],'utf-8',(error,responce)=>{
-    if(error){
-        console.log(error);
-    }
-    else{
-        console.log(responce);
-    }
+fs.readFile(process.argv[2], 'utf-8', (error, responce) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(responce);
+  }
 });
