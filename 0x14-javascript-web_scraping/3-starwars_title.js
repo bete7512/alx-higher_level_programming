8240bde17009bbf536ba8345fa9cfca9933e56af
bete7512 <betekbebe@gmail.com>
2022-10-18 21:16:08 +0300

    message for api fetching

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 'use strict';
 const request = require('request');
 
-request('http://swapi.co/api/films/' + process.argv[2], { json: true }, (error, response, b) => {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], { json: true }, (error, response, b) => {
   if (error) console.log(error); else {
     if (response.statusCode === 200) console.log(b.title);
   }
