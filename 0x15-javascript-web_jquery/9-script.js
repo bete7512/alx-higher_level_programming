c141adc20516c6bf316b2a5008d1e2ef8d6bc825
bete7512 <betekbebe@gmail.com>
2022-10-26 19:49:37 +0300

    change commited succefully

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,5 +1,5 @@
 $('document').ready(() => {
   $.get('https://fourtonfish.com/hellosalut/?lang=fr', (response) => {
-    $('DIV#hello').text(data.hello);
+    $('DIV#hello').text(response.hello);
   });
 });
